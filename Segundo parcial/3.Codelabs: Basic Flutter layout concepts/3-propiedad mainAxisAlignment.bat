c89d63452fa9211cccadddd1b96cffba06a18d1b
cleiva1988 <95687168+cleiva1988@users.noreply.github.com>
2023-04-07 18:42:02 -0600

    Create 3-propiedad mainAxisAlignment.bat
    Cuando mainAxisSizese establece en MainAxisSize.max, Rowy Columnpuede colocar a sus hijos con espacio adicional. La mainAxisAlignmentpropiedad determina cómo Rowy Column puede ubicar a sus hijos en ese espacio extra.

---
+++ Segundo parcial/3.Codelabs: Basic Flutter layout concepts/3-propiedad mainAxisAlignment.bat
@@ -0,0 +1,36 @@
+//Propiedad mainAxisAlignment
+
+//modificación de la alineación del eje principal
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      
+      //Posiciona a los niños cerca del final del eje principal. (Derecha para Row, abajo para Column)
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        BlueBox(),
+        BlueBox(),
+        BlueBox(),
+      ],
+    );
+  }
+}
+
+class BlueBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        border: Border.all(),
+      ),
+    );
+  }
+}
