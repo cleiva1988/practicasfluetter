17c90e912b7b42bd6836ce411f389383b1563c91
cleiva1988 <95687168+cleiva1988@users.noreply.github.com>
2023-04-07 19:53:28 -0600

    Create 13-creación de un icono.bat
    El Iconwidget muestra un símbolo gráfico que representa un aspecto de la interfaz de usuario. Flutter está precargado con paquetes de íconos para aplicaciones Material y Cupertino .

---
+++ Segundo parcial/3.Codelabs: Basic Flutter layout concepts/13-creación de un icono.bat
@@ -0,0 +1,37 @@
+//Widget de icono
+
+//creación de un icono
+
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      textBaseline: TextBaseline.alphabetic,
+      children: const [
+        Icon(
+          Icons.widgets,
+          size: 50,
+          color: Colors.blue,
+        ),
+        Icon(
+          Icons.widgets,
+          size: 50,
+          color: Colors.red,
+        ),  
+        
+ // Agregue otro Icon con un tamaño de 50 y color amber.
+        Icon(
+          Icons.widgets,
+          size: 50,
+          color: Colors.amber,
+        
+        ),
+      ],
+    );
+  }
+}
