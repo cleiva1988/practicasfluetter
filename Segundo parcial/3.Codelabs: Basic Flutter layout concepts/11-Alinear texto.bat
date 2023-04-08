498b87a5d81c3183363915d6d600fb736b53cefe
cleiva1988 <95687168+cleiva1988@users.noreply.github.com>
2023-04-07 19:41:07 -0600

    Create 11-Alinear texto.bat
    El siguiente ejemplo muestra "¡Oye!" tres veces, pero en diferentes tamaños de fuente y en diferentes colores. Rowespecifica las propiedades crossAxisAlignment y textBaseline.
    
    1. Haga clic en el botón Ejecutar .
    
    2. Cambie CrossAxisAlignment.centera CrossAxisAlignment.baseliney vuelva a ejecutar.

---
+++ Segundo parcial/3.Codelabs: Basic Flutter layout concepts/11-Alinear texto.bat
@@ -0,0 +1,45 @@
+//Widget de texto
+
+//Alinear texto
+
+//El siguiente ejemplo muestra "¡Hey!" tres veces, pero en diferentes tamaños de fuente y en diferentes colores. Rowespecifica las propiedades crossAxisAlignment y textBaseline.
+
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.baseline,
+      textBaseline: TextBaseline.alphabetic,
+      children: const [
+        Text(
+          'Hey!',
+          style: TextStyle(
+            fontSize: 30,
+            fontFamily: 'Futura',
+            color: Colors.blue,
+          ),
+        ),
+        Text(
+          'Hey!',
+          style: TextStyle(
+            fontSize: 50,
+            fontFamily: 'Futura',
+            color: Colors.green,
+          ),
+        ),
+        Text(
+          'Hey!',
+          style: TextStyle(
+            fontSize: 40,
+            fontFamily: 'Futura',
+            color: Colors.red,
+          ),
+        ),
+      ],
+    );
+  }
+}
