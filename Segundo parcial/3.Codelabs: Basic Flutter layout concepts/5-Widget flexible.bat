b6b82f2a8a6c355929b2329bd245c5ce5bf03784
cleiva1988 <95687168+cleiva1988@users.noreply.github.com>
2023-04-23 09:07:32 -0600

    Update 5-Widget flexible.bat

--- Segundo parcial/3.Codelabs: Basic Flutter layout concepts/5-Widget flexible.bat
+++ Segundo parcial/3.Codelabs: Basic Flutter layout concepts/5-Widget flexible.bat
@@ -1,8 +1,11 @@
 //Widget flexible
 
-import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+
+void main(){
+  runApp(MyWidget());
+}
 
 class MyWidget extends StatelessWidget {
   @override
