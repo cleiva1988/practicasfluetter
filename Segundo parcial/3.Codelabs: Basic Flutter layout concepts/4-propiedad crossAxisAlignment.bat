eeca3db59b7a565ffb344b878a7fe974c6f3cef0
cleiva1988 <95687168+cleiva1988@users.noreply.github.com>
2023-04-23 09:06:34 -0600

    Update 4-propiedad crossAxisAlignment.bat

--- Segundo parcial/3.Codelabs: Basic Flutter layout concepts/4-propiedad crossAxisAlignment.bat
+++ Segundo parcial/3.Codelabs: Basic Flutter layout concepts/4-propiedad crossAxisAlignment.bat
@@ -1,8 +1,11 @@
 // propiedad crossAxisAlignment
 
-import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+
+void main(){
+  runApp(MyWidget());
+}
 
 class MyWidget extends StatelessWidget {
   @override
