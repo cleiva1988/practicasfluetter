c700ada23cb7131a625e6c3ca9b1debb0aa49a07
cleiva1988 <95687168+cleiva1988@users.noreply.github.com>
2023-04-23 08:59:17 -0600

    primer commit
    adicional run App

--- Segundo parcial/3.Codelabs: Basic Flutter layout concepts/14-Poniendo todo junto.bat
+++ Segundo parcial/3.Codelabs: Basic Flutter layout concepts/14-Poniendo todo junto.bat
@@ -1,8 +1,11 @@
 //interfaz de usuario de Flutter que muestre una tarjeta de presentación
 
-import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+
+void main(){
+  runApp(MyWidget());
+}
 
 class MyWidget extends StatelessWidget {
   @override
