d51500630b25d0979042c1f06ffc294f89dca9dd
cleiva1988 <95687168+cleiva1988@users.noreply.github.com>
2023-04-27 20:43:38 -0600

    Update 11-Alinear texto.bat

--- Segundo parcial/3.Codelabs: Basic Flutter layout concepts/11-Alinear texto.bat
+++ Segundo parcial/3.Codelabs: Basic Flutter layout concepts/11-Alinear texto.bat
@@ -7,8 +7,18 @@
 
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MyWidget());
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyWidget()
+    );
+    throw UnimplementedError();
+  }
 }
 
 class MyWidget extends StatelessWidget {
